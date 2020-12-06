9a86618d8677ba2b68fe40e6b0e540cfcd5dca53
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-12-06 17:52:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 		<body>
 			<body bgcolor="red">
 		Hello world
-		<img src="g1.png">
-		
+		<img src="g1.png" lenght:100; width:100>
+				<a href="https://www.youtube.com/watch?v=sPOgNmbBAX0"></a>
 		
 		
 		
